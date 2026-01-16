321ffc84a870379b931313f0eda90da91c6ae31f
Xander-Henderson <xander.henderson@stu.ocsb.ca>
2026-01-16 12:41:46 -0500

    fixed bugs, made bugs

--- main.ts
+++ main.ts
@@ -13,7 +13,7 @@
 let sendSignalStoped: number = 0
 let wallCounter: number = 0
 let soundLevel: number = 0
-let distanceToObject: number = 10
+let distanceToObject: number = 0
 let xandersNeopixelStrip: neopixel.Strip = null
 
 // setup
@@ -28,37 +28,39 @@ basic.showIcon(IconNames.Cow)
 
 // main function, makes the tank move forward
 function moveUntilWall() {
-    if (distanceToObject >= 5) {
+    if (distanceToObject > 5) {
         xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
         xandersNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
         xandersNeopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
         xandersNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
         xandersNeopixelStrip.show()
-         robotbit.StpCarMove(2, 2.5)
-    }
-        
-    if (distanceToObject <= 5) {
+         robotbit.StpCarMove(5, 48)
+    } 
+    
+    if (distanceToObject < 5) {
         sendSignalStoped = 1
+        wallCounter = 0
     }
 }
 
 while (true) {
-    // gets the distance and sound level
+    // gets the distance and the sound level
     soundLevel = input.soundLevel()
+
     distanceToObject = sonar.ping(
         DigitalPin.P14,
         DigitalPin.P15,
         PingUnit.Centimeters
     )
 
-    basic.showString(distanceToObject.toString())
-
-    if (soundLevel >= 10) {
+    if (soundLevel > 100) {
         wallCounter = 1
     }
+    
     if (wallCounter = 1 ) {
-        moveUntilWall
+        moveUntilWall()
     }
+
     if (sendSignalStoped = 1) {
         xandersNeopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
         xandersNeopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
@@ -66,14 +68,18 @@ while (true) {
         xandersNeopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
         xandersNeopixelStrip.show()
 
+        wallCounter = 0
+        sendSignalStoped = 0
+
         input.onButtonPressed(Button.A, function() {
-             robotbit.StpCarTurn(-90, 2.5, 105)
-            sendSignalStoped = 0
+             robotbit.StpCarTurn(-90, 48, 105)
+            wallCounter = 1
         })
 
         input.onButtonPressed(Button.B, function() {
-             robotbit.StpCarTurn(90, 2.5, 105)
-            sendSignalStoped = 0
+             robotbit.StpCarTurn(90, 48, 105)
+            wallCounter = 1
         })
+        basic.pause(50)
     }
 }
